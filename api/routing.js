08992c064a4b4a11d561026e8283bb228e235f3b
k4noise <vestertp@gmail.com>
2021-02-03 23:03:22 +0500

    feat: новый роутер

--- api/routing.js
+++ api/routing.js
@@ -1,10 +1,30 @@
-const routing = {
+// const routing = {
+//   '/api/register': api.register,
+//   '/api/login': api.login,
+//   '/api/notes': api.notes,
+//   '/api/addNote': api.addNotes,
+// };
+const routing2 = {
+  '*': staticFiles,
+  '/': staticFiles('index.html'),
+  '/index': staticFiles('index.html'),
+  '/app': staticFiles('app.html'),
   '/api/register': api.register,
   '/api/login': api.login,
   '/api/notes': api.notes,
   '/api/addNote': api.addNotes,
 };
 
-const shortUrl = { '/': 'index.html', '/app': 'app.html' };
+// const shortUrl = { '/': 'index.html', '/app': 'app.html' };
 
-module.exports = { routing, shortUrl };
+// module.exports = { routing, shortUrl };
+
+const router = async (body) => {
+  let result = routing2[body.url] || routing2['*'](body.url);
+  if (typeof result === 'function') {
+    result = await result(body);
+  }
+  return result;
+};
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ global.salt = process.env.salt;
 global.http = require('http');
 global.fs = require('fs');
 global.path = require('path');
-global.staticFile = require('./api/static');
+global.staticFiles = require('./api/static');
 global.postReceiver = require('./api/postReceiver');
 global.isValidObject = require('./api/validate');
 global.bcrypt = require('bcrypt');
@@ -15,28 +15,23 @@ api.register = require('./api/register');
 api.login = require('./api/login');
 api.notes = require('./api/notes');
 api.addNotes = require('./api/addNote');
-const { routing, shortUrl } = require('./api/routing');
-api.routing = routing;
-api.shortUrl = shortUrl;
+// const { routing, shortUrl } = require('./api/routing');
+// api.routing = routing;
+// api.shortUrl = shortUrl;
 api.searchUser = require('./api/searchUser');
 
+global.router = require('./api/routing');
+
 const requestHandler = async (req, res) => {
-  let body = req.url;
+  const body = {};
+  body.url = req.url;
   if (req.method !== 'GET') {
-    body = await postReceiver(req);
+    body += await postReceiver(req);
   }
-  if (api.routing[req.url]) {
-    const result = await api.routing[req.url](body);
-    if (req.url === '/api/register') {
-      res.setHeader('Set-Cookie', `token=${result.body.token}; HttpOnly`);
-    }
-    res.writeHead(result.status, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(result.body));
-  } else {
-    const file = await staticFile(api.shortUrl[req.url] || req.url);
-    res.writeHead(200, { 'Content-Type': file.mime });
-    res.end(file.body);
-  }
+  const file = await router(body);
+  res.status = file.status;
+  res.setHeader('Content-Type', file.mime || 'application/json');
+  res.end(file.body);
 };
 
 http.createServer(requestHandler).listen(8125);
